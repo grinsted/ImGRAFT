d56c04e5f2df244455b502f65714f2a9f69a5178
Aslak Grinsted <ag@glaciology.net>
2014-12-17 08:36:45 +0100

    test existence for a specific demo file

--- downloadDemoData.m
+++ downloadDemoData.m
@@ -48,19 +48,22 @@ switch lower(package)
     case {'cias' 'muragl' 'batura'}
         url='http://imgraft.glaciology.net/documentation/examples/cias-example/cias_data.zip';
         targetdir=fullfile(path,'cias_data');
+        testfile=fullfile(targetdir,'batura_2000.tif');
     case {'practise' 'schneefernerkopf' 'schneeferner'}
         url='http://imgraft.glaciology.net/documentation/examples/schneefernerkopf-example/practise-schneeferner-example.zip';
         targetdir=fullfile(path,'practise_data');
+        testfile=fullfile(targetdir,'dem30m.mat');
     case {'imcorr' 'bindschadler' 'icestreamd'}
         url='http://imgraft.glaciology.net/documentation/examples/imcorr-examples/imcorr-exampledata.zip';
         targetdir=fullfile(path,'imcorr_data');
+        testfile=fullfile(targetdir,'conv_89.png');
     otherwise
-        error('unknown package')
+        error('unknown download package')
 end
        
 fname=fullfile(path,'_temp.zip');
         
-if length(dir(targetdir))<3
+if ~exist(testfile,'file')
     s=urlread(url); %matlab's urlread/urlwrite does not comply with the "moved" header. Forced to do this hack... (Note this may be fragile)
     url=regexpi(s,'href="([^\n"]+)"','once','tokens');
     url=url{1};
