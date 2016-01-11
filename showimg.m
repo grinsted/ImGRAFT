3bb22e8083014edba351153cce71d8d2675b1d23
Aslak Grinsted <ag@glaciology.net>
2016-01-11 14:20:52 +0100

    added showimg

---
+++ showimg.m
@@ -0,0 +1,22 @@
+function h=showimg(x,y,A)
+% Display an image
+%
+% Usage h=showimg(x,y,A)
+%
+%
+% Aslak Grinsted 2015
+
+
+if ~isa(A,'uint8')
+    A=A-min(A(:));
+    A=uint8(A*255.49./max(A(:)));
+end
+if size(A,3)==1
+    A=repmat(A,[1 1 3]);
+end
+
+h=image(A,'XData',x,'YData',y,'CDataMapping','scaled') %the cdatamapping is a workaround for a bug in R2014+
+axis equal tight xy off
+if nargout==0
+    clearvars h
+end
