9dac239999fcfbc62c73fb5cda2b009b43f08745
Aslak Grinsted <ag+github@glaciology.net>
2018-05-24 13:05:45 +0200

    allow for missing x,y

--- showimg.m
+++ showimg.m
@@ -6,6 +6,12 @@ function h=showimg(x,y,A)
 %
 % Aslak Grinsted 2016
 
+if nargin==1
+    A=x;
+    x=1:size(A,2);
+    y=1:size(A,1);
+end
+
 if ~isa(A,'uint8')
     A=A-min(A(:));
     A=uint8(A*255.49./max(A(:)));
@@ -16,6 +22,13 @@ end
 
 h=image(A,'XData',x,'YData',y,'CDataMapping','scaled'); %the cdatamapping is a workaround for a bug in R2014+
 axis equal tight xy off
+if nargin==1
+    axis ij
+end
 if nargout==0
     clearvars h
 end
+
+
+
+
